3353c14f4d45c5ac8a098e05b36432d2501307c6
eddie4k <edwardoc@buffalo.edu>
2024-06-23 21:09:56 -0400

    Add extension to add services

---
+++ src/Core/HRManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HRManagement.Application
+{
+
+    /* 
+    
+    Extend the IServiceCollection to create a method that allows us to add application services 
+    returns services we want to add
+    */
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServies(this IServiceCollection services) {
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly()); //automatically register
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+
+
+        }
+    }
+}
